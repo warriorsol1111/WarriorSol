2cf13a2d5c842df1096f036a8a23a6b9921d4706
areeb-saqib <areeb.saqib@ccript.com>
2025-07-31 19:02:57 +0500

    feat: verify email pages update

--- apps/warriorsol-foundation/src/components/auth/verify-email.tsx
+++ apps/warriorsol-foundation/src/components/auth/verify-email.tsx
@@ -1,31 +1,27 @@
 "use client";
 
 import Image from "next/image";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import SignupImage from "@/assets/auth/signup.svg";
+import VerifyEmailImage1 from "@/assets/auth/verifyEmail1.svg";
+import VerifyEmailImage2 from "@/assets/auth/verifyEmail2.svg";
 import Logo from "@/assets/auth/logo.svg";
 import { toast } from "react-hot-toast";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
-import clsx from "clsx";
+import { OtpInput } from "reactjs-otp-input";
 
 export default function VerifyEmailPage() {
   const [verificationCode, setVerificationCode] = useState("");
   const [errors, setErrors] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [resending, setResending] = useState(false);
+  const [step, setStep] = useState(1);
 
   const router = useRouter();
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setVerificationCode(e.target.value);
-    setErrors(null); // clear error on change
-  };
 
   const validateCode = (code: string) => {
     const trimmed = code.trim();
@@ -45,14 +41,11 @@ export default function VerifyEmailPage() {
 
     try {
       setLoading(true);
-
       const response = await fetch(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/verify-email`,
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, verificationCode }),
         }
       );
@@ -61,7 +54,7 @@ export default function VerifyEmailPage() {
 
       if (data.status === "success") {
         toast.success("Email verified successfully");
-        router.push("/login");
+        setStep(2);
       } else {
         toast.error("Invalid verification code");
       }
@@ -80,9 +73,7 @@ export default function VerifyEmailPage() {
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/resend-otp`,
         {
           method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, type: "email" }),
         }
       );
@@ -104,89 +95,113 @@ export default function VerifyEmailPage() {
 
   return (
     <div className="flex min-h-screen flex-col md:flex-row">
-      {/* Left Section */}
+      {/* Left Side */}
       <div className="flex w-full items-center justify-center bg-white p-6 md:w-1/2 md:p-12 md:px-24">
-        <form
-          onSubmit={handleSubmit}
-          className="mt-12 w-full space-y-4 md:space-y-6"
-        >
-          <div className="text-center md:text-left">
-            <h1 className="font-serif text-3xl font-normal md:text-5xl">
-              Verify Your Email
+        {step === 1 ? (
+          <form onSubmit={handleSubmit} className="mt-12 w-full space-y-6">
+            <div className="text-center md:text-left">
+              <h1 className="font-[Cormorant SC] text-3xl md:text-[42px]">
+                Email Verification
+              </h1>
+              <p className="mt-2 text-base font-light text-[#1F1F1F99] font-[Inter] md:text-lg">
+                We have sent a verification code on your Email
+              </p>
+              <p className="text-base  text-[#EE9254] font-[Inter] md:text-lg">
+                {email}
+              </p>
+            </div>
+            <div className="space-y-4">
+              <Label htmlFor="verificationCode">Please Enter Code</Label>
+              <OtpInput
+                value={verificationCode}
+                onChange={(val) => {
+                  setVerificationCode(val);
+                  setErrors(null);
+                }}
+                numInputs={6}
+                isInputNum
+                shouldAutoFocus
+                inputStyle={{
+                  width: "100%",
+                  height: "3rem",
+                  fontSize: "1.25rem",
+                  borderRadius: "0.375rem",
+                  border: `1px solid ${errors ? "#ef4444" : "#d1d5db"}`,
+                  outline: "none",
+                  textAlign: "center",
+                }}
+                containerStyle={{
+                  display: "flex",
+                  gap: "0.5rem",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              />
+              {errors && <p className="text-sm text-red-500 -mt-2">{errors}</p>}
+            </div>
+
+            <Button
+              type="submit"
+              disabled={loading}
+              className="h-12 w-full bg-[#EE9254] text-white hover:bg-[#EE9254] text-lg font-[Inter] flex items-center justify-center gap-2"
+            >
+              {loading && <Loader2 className="h-5 w-5 animate-spin" />}
+              Verify Email
+            </Button>
+
+            <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-y-0">
+              <p className="text-center text-base font-light md:text-lg">
+                Didn&apos;t receive the code?{" "}
+                <Button
+                  type="button"
+                  variant="ghost"
+                  onClick={handleResendCode}
+                  disabled={resending}
+                  className="px-1 text-base font-bold text-black underline"
+                >
+                  {resending && (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin text-black" />
+                  )}
+                  Resend Code
+                </Button>
+              </p>
+            </div>
+
+            <div className="flex justify-center">
+              <p className="text-base font-light">
+                Back to{" "}
+                <a href="/login" className="font-bold underline text-black">
+                  Login
+                </a>
+              </p>
+            </div>
+          </form>
+        ) : (
+          <div className="mt-12 w-full text-center space-y-6">
+            <h1 className="text-[#1F1F1F] text-3xl md:text-[42px] font-[Cormorant SC]">
+              Account Created <br />
+              Successfully
             </h1>
-            <p className="mt-2 text-base font-light md:text-lg">
-              Please enter the verification code sent to your email address
-            </p>
-          </div>
 
-          <div className="space-y-4">
-            <Label htmlFor="verificationCode">Verification Code</Label>
-            <Input
-              id="verificationCode"
-              name="verificationCode"
-              type="text"
-              value={verificationCode}
-              onChange={handleChange}
-              placeholder="Enter your verification code"
-              className={clsx(
-                "w-full",
-                errors && "border-red-500 focus:ring-red-300"
-              )}
-            />
-            {errors && (
-              <p className="text-sm text-red-500 -mt-2">{errors}</p>
-            )}
+            <Button
+              onClick={() => router.push("/login")}
+              className="h-12 w-full hover:bg-[#EE9254] bg-[#EE9254] text-white font-[Inter] text-xl"
+            >
+              Back to Login
+            </Button>
           </div>
-
-          <Button
-            type="submit"
-            disabled={loading}
-            className="h-12 w-full cursor-pointer bg-[#EE9254] text-white hover:bg-[#EE9254] flex items-center justify-center gap-2"
-          >
-            {loading && <Loader2 className="h-5 w-5 animate-spin" />}
-            Verify Email
-          </Button>
-
-          <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-y-0">
-            <p className="text-center text-base font-light md:text-lg">
-              Didn&apos;t receive the code?{" "}
-              <Button
-                type="button"
-                variant="ghost"
-                onClick={handleResendCode}
-                disabled={resending}
-                className="cursor-pointer px-1 text-base font-bold text-black underline md:text-lg"
-              >
-                {resending && (
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin text-black" />
-                )}
-                Resend Code
-              </Button>
-            </p>
-          </div>
-
-          <div className="flex justify-center gap-x-4">
-            <p className="text-center text-base font-light md:text-lg">
-              Back to{" "}
-              <a href="/login" className="font-bold text-black underline">
-                Login
-              </a>
-            </p>
-          </div>
-        </form>
+        )}
       </div>
 
-      {/* Right Section */}
+      {/* Right Side */}
       <div className="relative h-[300px] w-full md:h-auto md:w-1/2">
         <Image
-          src={SignupImage}
+          src={step === 1 ? VerifyEmailImage1 : VerifyEmailImage2}
           alt="Verify Email Background"
           fill
           className="object-cover"
           priority
         />
-
-        {/* Overlay Content */}
         <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/40 px-4 text-center text-white">
           <Image
             src={Logo}
